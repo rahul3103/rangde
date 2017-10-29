607b167899158fa956bfa08ef81719fd948e02d9
Rahul Shrivastava <rahul3103@gmail.com>
2017-10-29 23:52:22 +0530

    removed some files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Container from './components/container';
 import registerServiceWorker from './registerServiceWorker';
 
